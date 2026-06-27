ada80eebc237f7dd861e00a6a552b7bbd7e70c54
aliqannan <aliibrhemqannan>
2026-06-27 21:10:42 +0800

    new

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // This forces all generated URLs (assets, routes) to use https://
+        URL::forceScheme('https');
     }
 }
